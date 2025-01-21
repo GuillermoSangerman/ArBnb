dbaf42e2a0d90cbfefd761d4478e8480a6de2644
Guillermo Sangerman <sangermanmptn8@gmail.com>
2025-01-20 23:55:55 -0600

    se establecio estructura

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 export default function App() {
   return (
-    <h1 className="text-3xl text-white underline bg-black">
-      Hello world!
-    </h1>
+   <>
+      <nav>
+        <div className="m-5"><img src="https://joregesosa.github.io/windbnb_challenge/assets/logo-f7862584.svg" alt="iconoArbnb" /></div>
+        <ul className="flex items-center justify-center border border-black rounded-xl mx-14 ">
+          <li className="border border-black border-s-0 border-y-0">Add location</li>
+          <li className="border border-black border-s-0 border-y-0">Add guests</li>
+          <li className="underline decoration-pink-500"><img className="h-5 underline decoration-pink-500" src="\src\assets\design\lupa.svg" alt="imagen lupa" /></li>
+        </ul>
+      </nav>
+      <header>
+        <h1>h1</h1>
+        <h2>h2</h2>
+      </header>
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <span>superhost</span>
+          <span>Lorem ipsum dolor sit</span>
+          <span>⭐</span>
+          <div><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odit?</span></div>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <span>superhost</span>
+          <span>Lorem ipsum dolor sit</span>
+          <span>⭐</span>
+          <div><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odit?</span></div>
+        </li>
+
+      </ul>
+   </>
   )
 }
